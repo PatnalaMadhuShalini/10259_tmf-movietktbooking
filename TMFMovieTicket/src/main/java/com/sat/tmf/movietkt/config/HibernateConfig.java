361bdc3f50e1acbb25ec620afd1e7d3a438401bb
git <madhushalinipatnala@gmail.com>
2025-11-23 13:47:37 +0530

    update

--- TMFMovieTicket/src/main/java/com/sat/tmf/movietkt/config/HibernateConfig.java
+++ TMFMovieTicket/src/main/java/com/sat/tmf/movietkt/config/HibernateConfig.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -14,7 +16,7 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@Configuration
+@Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
 public class HibernateConfig {
@@ -34,13 +36,14 @@ public class HibernateConfig {
     }
 	
     @Autowired
+    @Lazy
     private DataSource dataSource;
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
-        sessionFactory.setPackagesToScan("com.movieticketbooking.model");
+        sessionFactory.setPackagesToScan("com.sat.tmf.movietkt.entities");
         Properties props = new Properties();
         props.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         props.put("hibernate.show_sql", true);
